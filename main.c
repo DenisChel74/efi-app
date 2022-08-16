aa500297614616247d40a94483e37c39d4491817
hermione <hermione@hog.uk>
2022-08-16 18:14:08 +0500

    save

--- main.c
+++ main.c
@@ -12,16 +12,17 @@ EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     InitializeLib(ImageHandle, SystemTable);
-    lv_init();
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    lv_disp_draw_buf_init(&disp_buf, buf1, NULL, W_RESOLUTION * H_RESOLUTION); /*Initialize the display buffer.*/
-    lv_disp_drv_register(&disp_drv);
+//    lv_init();
+//    static lv_disp_drv_t disp_drv;
+//    lv_disp_drv_init(&disp_drv);
+//    lv_disp_draw_buf_init(&disp_buf, buf1, NULL, W_RESOLUTION * H_RESOLUTION); /*Initialize the display buffer.*/
+//    lv_disp_drv_register(&disp_drv);
     Print(L"DEBUG: INITIALIZE APPLICATION");
     while (TRUE) {
-        lv_tick_inc(5);
-        lv_task_handler();
-        gBS->Stall(1000 * 5);
+//        lv_tick_inc(5);
+//        lv_task_handler();
+//        gBS->Stall(1000 * 5);
+        Print(L"tick");
     }
     return EFI_SUCCESS;
 }
--- public.sh
+++ public.sh
@@ -1,1 +1,14 @@
+sudo mount -t vfat -o loop,offset=$((2048 * 512)) /var/lib/libvirt/images/efitester.img ~/efitester
 sudo cp /home/hermione/CLionProjects/EFIFrontend/cmake-build-debug/bin/bootx64.efi ~/efitester/EFI/BOOT/bootx64.efi
+sudo umount ~/efitester/
+
+OVMF_DISK_IMG=/usr/share/ovmf/x64/OVMF.fd
+DISK_PATH=/var/lib/libvirt/images/efitester.img
+
+# run
+sudo qemu-system-x86_64 \
+  -cpu qemu64 \
+  -enable-kvm \
+  -net none \
+  -drive if=pflash,format=raw,unit=0,file=${OVMF_DISK_IMG},readonly=on \
+  -drive if=ide,format=raw,file=/var/lib/libvirt/images/efitester.img
